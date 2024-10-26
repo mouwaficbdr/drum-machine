c4aea48fd25e9da767016cd8a615d8901ee0000a
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-10-27 00:26:49 +0100

    Initialization

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/no-deprecated */
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
